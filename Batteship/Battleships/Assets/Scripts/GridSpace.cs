2b0e9801d189655d747b636808907a52adcd76be
Angemar450 <marzecangela@gmail.com>
2018-05-22 19:14:09 +0200

    Button support

---
+++ Batteship/Battleships/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    public string playerChoice;
+    public void SetSpace()
+    {
+        buttonText.text = playerChoice;
+        button.interactable = false;
+    }
+}
---
+++ Batteship/Battleships/Assets/Scripts/GridSpaceEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpaceEnemy : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    public string playerChoice;
+
+    public void SetSpace()
+    {
+        buttonText.text = playerChoice;
+        button.interactable = false;
+
+    }
+
+}
